0ea64241f524acd77b3d47fdc0156fbe3ff0ba44
Mr-Morat <mtarom124@gmail.com>
2025-09-18 03:21:10 +0000

    sweet: add 90Hz panel support with backlight adjustments
    - Enable 90Hz panel mode on sweet
    - Adapt device tree and configuration for stable 90Hz operation
    - Adjust minimum backlight level for better low brightness usability
    - Recalculate brightness scaling to match 90Hz requirements
    
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2210,6 +2210,7 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-dispparam-60hz-dc-crc-setting-command",
 	"qcom,mdss-dsi-dispparam-120hz-dc-crc-setting-command",
 	"qcom,mdss-dsi-dispparam-bc-120hz-command",
+	"qcom,mdss-dsi-dispparam-bc-90hz-command",
 	"qcom,mdss-dsi-dispparam-bc-60hz-command",
 };
 
@@ -2304,6 +2305,7 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-dispparam-60hz-dc-crc-setting-command-state",
 	"qcom,mdss-dsi-dispparam-120hz-dc-crc-setting-command-state",
 	"qcom,mdss-dsi-dispparam-bc-120hz-command-state",
+	"qcom,mdss-dsi-dispparam-bc-90hz-command-state",
 	"qcom,mdss-dsi-dispparam-bc-60hz-command-state",
 };
 
@@ -5752,12 +5754,18 @@ int panel_disp_param_send_lock(struct dsi_panel *panel, int param)
 		pr_info("BC 120hz\n");
 		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_BC_120HZ);
 		break;
-	case DISPPARAM_BC_60HZ:
-		pr_info("BC 60hz\n");
-		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_BC_60HZ);
-		break;
 	default:
 		break;
+	}
+
+	temp = param & 0x000F0000;
+	if (temp == DISPPARAM_BC_60HZ) {
+		pr_info("BC 60hz\n");
+		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_BC_60HZ);
+	} else if (temp == DISPPARAM_BC_90HZ) {
+		pr_info("BC 90hz\n");
+		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_BC_90HZ);
+	} else {
 	}
 
 	temp = param & 0x00F00000;
