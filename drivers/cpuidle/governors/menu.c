4ca156ad307203559f1298ee333a9ea23f8908b4
Mr-Morat <mtarom124@gmail.com>
2026-03-22 22:17:53 +0700

    cpuidle: sync lpm-levels and related power optimizations
    Sync and integrate lpm-levels updates along with related cpuidle and power management optimizations
    
    - lpm-levels improvements and cleanup
    - enforcement logic for deepest cluster idle state (adapted)
    - removal of tracing from hot paths
    - cleanup of legacy cpuidle code paths
    - switch to Qualcomm idle governor (menu/ladder disabled)
    - minor scheduler idle balance adjustments
    
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -21,7 +21,6 @@
 #include <linux/sched/loadavg.h>
 #include <linux/sched/stat.h>
 #include <linux/math64.h>
-#include <linux/cpu.h>
 
 /*
  * Please note when changing the tuning values:
@@ -524,7 +523,7 @@ static void menu_update(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 		measured_us = 9 * MAX_INTERESTING / 10;
 	} else {
 		/* measured value */
-		measured_us = cpuidle_get_last_residency(dev);
+		measured_us = dev->last_residency;
 
 		/* Deduct exit latency */
 		if (measured_us > 2 * target->exit_latency)
